be19f09aed40c19a0229953a752782c458f3f367
Panconpeenga <imsoto@icloud.com>
2018-04-27 15:25:49 -0400

    updated all files to actual background generator

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-//helloksjdbkajbsdkaj
-// osnadon ksjdna
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var random = '#'+Math.floor(Math.random()*16777215).toString(16);
+var button = document.querySelector("button");
+
+setGradient();
+
+function setGradient() {
+	body.style.background =
+	"linear-gradient(to right, "
+	+ color1.value
+	+ ", "
+	+ color2.value
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+button.addEventListener("click", function(){
+  button.innerHTML = '#'+Math.floor(Math.random()*16777215).toString(16);
+});
